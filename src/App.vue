332aaeb143eeafb5b4c6cb03584c52e93a7a4669
Roman <unforgiven18-ka@ukr.net>
2018-09-13 13:29:03 +0300

    com

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ $t("message.hello") }}</p>
+    <component1></component1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import router from './router'
+    import Component1 from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {Component1},
+        created: function () {
+            var localeFull = navigator.language;
+            let locale = 'uk';
+            if (localeFull.match(/en/)) {
+                locale = 'en';
+            } else if (localeFull.match(/ru/)) {
+                locale = 'ru';
+            }
+            router.push({path: `/${locale}`})
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
