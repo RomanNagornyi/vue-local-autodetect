2f10ea41fcab2ea23d089243f7f6536bad1c5bce
Roman <unforgiven18-ka@ukr.net>
2018-09-17 12:32:50 +0300

    com

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
     <p>{{ $t("message.hello") }}</p>
-    <component1></component1>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
     import router from './router'
-    import Component1 from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
     export default {
         name: 'app',
-        components: {Component1},
+        components: {HelloWorld},
         created: function () {
-            var localeFull = navigator.language;
-            let locale = 'uk';
-            if (localeFull.match(/en/)) {
+            var localeFull = navigator.languages ? navigator.languages : [(navigator.language || navigator.userLanguage)];
+            let locale = 'en';
+            if (localeFull.findIndex(s => s.includes('uk')) > -1) {
+                locale = 'uk';
+            } else if (localeFull.findIndex(s => s.includes('ru')) > -1 && localeFull.findIndex(s => s.includes('en')) > -1) {
                 locale = 'en';
-            } else if (localeFull.match(/ru/)) {
+            } else if (localeFull.findIndex(s => s.includes('ru')) > -1) {
                 locale = 'ru';
             }
             router.push({path: `/${locale}`})
